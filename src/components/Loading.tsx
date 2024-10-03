52f6f4e8a90adaffc38e71920465bd39008625db
MrMirror21 <mylittlesentry@gmail.com>
2024-10-03 15:02:45 +0000

    feat - 로딩 컴포넌트 개선

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,35 +1,17 @@
-import React, { useState } from 'react';
-
 import Image from 'next/image';
+import React from 'react';
 
 const Loading = () => {
-  const [isVisible, setIsVisible] = useState(true);
-
   return (
-    <div>
-      <div className="relative h-screen">
-        <div
-          className={`fixed inset-0 bg-white transition-opacity duration-1000 ease-in-out flex flex-col ${
-            isVisible ? 'bg-opacity-50' : 'bg-opacity-0 pointer-events-none'
-          }`}
-        >
-          <div
-            className={`transform transition-all duration-300 absolute top-1/2 left-1/2 -translate-x-1/2 ${
-              isVisible
-                ? '-translate-y-1/2 opacity-100'
-                : 'translate-y-full opacity-0'
-            }`}
-          >
-            <Image
-              src="/gif/LoadingGIFDinoReal.gif"
-              alt="loading"
-              width={80}
-              height={80}
-              className="z-[9999]"
-              priority
-            />
-          </div>
-        </div>
+    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-50 z-50">
+      <div className="relative">
+        <Image
+          src="/gif/LoadingGIFDinoReal.gif"
+          alt="loading"
+          width={70}
+          height={70}
+          priority
+        />
       </div>
     </div>
   );
