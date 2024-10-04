ce0579a92228bfc8afe17244df70981537e1ba62
MrMirror21 <mylittlesentry@gmail.com>
2024-10-04 05:12:11 +0000

    fix : 화면 벗어남 처리 2회차

--- src/components/common/MainLayout.tsx
+++ src/components/common/MainLayout.tsx
@@ -1,7 +1,7 @@
 const MainLayout = (props: { children: React.ReactNode }) => {
   return (
-    <div className="fixed inset-0 flex justify-center bg-white">
-      <div className="w-full max-w-[480px] h-full overflow-y-auto">
+    <div className="fixed inset-0 flex justify-center bg-white overflow-hidden">
+      <div className="w-full max-w-[480px] h-full overflow-y-auto overflow-x-hidden">
         {props.children}
       </div>
     </div>
